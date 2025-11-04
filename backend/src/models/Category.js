5e30e19543dcd7c95bf6aac199a1c3003b134a0d
Trần Hoàng Hảo <tranhao201103@gmail.com>
2025-11-04 11:41:23 +0700

    Delete backend directory

--- backend/src/models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String }, // Thêm trường mô tả, không bắt buộc
-  subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject", required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Category", categorySchema);
--- backend/src/models/Subject.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const subjectSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Subject", subjectSchema);
